0e29b78308df02f30e020a24d2b2c8b329318ea2
paragaggarwal <paragaggarwal04@gmail.com>
2023-08-15 16:00:58 +0530

    Hotel Reservation App Frontend

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,45 @@
+import "./footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="fList">
+                <ul className="fList">
+                    <li className="fList">Countries</li>
+                    <li className="fList">Regions</li>
+                    <li className="fList">Cities</li>
+                    <li className="fList">Districts</li>
+                    <li className="fList">Airports</li>
+                    <li className="fList">Hotels</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fList">Homes</li>
+                    <li className="fList">Apartments</li>
+                    <li className="fList">Resorts</li>
+                    <li className="fList">Villas</li>
+                    <li className="fList">Hostels</li>
+                    <li className="fList">Guest houses</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fList">Car rental</li>
+                    <li className="fList">Flight Finder</li>
+                    <li className="fList">Restaurants reservations</li>
+                    <li className="fList">Travel Agents</li>
+                </ul>
+                <ul className="fList">
+                    <li className="fList">Customer Services</li>
+                    <li className="fList">Partner Help</li>
+                    <li className="fList">Carrers</li>
+                    <li className="fList">Sustainability</li>
+                    <li className="fList">Press center</li>
+                    <li className="fList">Safety Resource Center</li>
+                    <li className="fList">Investors relations</li>
+                    <li className="fList">Terms & conditions</li>
+                </ul>
+            </div>
+            <div className="fText">Copyright @ 2023 Booking</div>
+        </div>
+    );
+};
+
+export default Footer;
